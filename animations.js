4e2eebc73fb6415e5739665955590ad03f6190ec
lxrkii, but how much.... <74319639+lxrkii@users.noreply.github.com>
2024-12-19 16:28:44 +0300

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
     form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Prevent the default form submission
-        alert('Form submitted with cool animation!'); // Replace with your animation logic
-        // You can add more animation logic here
+        event.preventDefault(); 
+        alert('Form submitted with cool animation!'); 
     });
 });
